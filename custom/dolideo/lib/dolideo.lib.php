66a949deece9d6d048cefee1f4e88792b05dccf5
Frederic Omega Junior <omegajunior.apps@gmail.com>
2024-02-29 16:30:09 +0100

    ignore

--- custom/dolideo/lib/dolideo.lib.php
+++ custom/dolideo/lib/dolideo.lib.php
@@ -1,5 +1,5 @@
 <?php
-/* Copyright (C) 2024 SuperAdmin
+/* Copyright (C) 2024 omega junior
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
